c32977cf20c674e0c3f4a79a16af50906b6b92fc
DESKTOP-CEKMC59\weiwudi <2374779862@qq.com>
2020-11-13 22:37:47 +0800

    no message

---
+++ booksource/FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsContentActivity.java
@@ -0,0 +1,14 @@
+package com.example.fragmentbestpractice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewsContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_content);
+    }
+}
