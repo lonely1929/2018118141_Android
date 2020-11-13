9886e96237348543d3eab645cf76b4287d34a8ac
DESKTOP-CEKMC59\weiwudi <2374779862@qq.com>
2020-11-13 21:44:30 +0800

    Intent应用

---
+++ 4-Homework/4-Homework.md
@@ -0,0 +1,41 @@
+## 实验要求：熟悉Intent应用
+## 实验目的：通过实验了解Intent应用，特别是系统内置隐含Intent和自定义的Intent的使用
+## 实验内容：
+
+​	1.显式Intent：在FirstActivity的界面点击一下按钮就会启动SecondActivity活动。
+
+![image-20201113173544331](4-Homework.assets/image-20201113173544331.png)
+
+
+
+​	2.隐式Intent：在FirstActivity的界面点击一下按钮就可以打开浏览器。
+
+![image-20201113205200293](4-Homework.assets/image-20201113205200293.png)
+
+
+
+​	3.隐式Intent：ThirdActivity应该和浏览器一样，能够响应一个打开网页的Intent，在FirstActivity的界面点击一下按钮，结果如下图所示。
+
+![image-20201113210049897](4-Homework.assets/image-20201113210049897.png)
+
+
+
+​	4.隐式Intent：在FirstActivity的界面点击一下按钮就可以打开系统拨号界面。
+
+![image-20201113210328040](4-Homework.assets/image-20201113210328040.png)
+
+
+
+​	5.向下一个活动传递数据：在SecondActivity中成功得到了从FirstActivity传递过来的数据。
+
+![image-20201113210857608](4-Homework.assets/image-20201113210857608.png)
+
+
+
+6.返回数据给上一个活动：SecondActivity已经成功返回数据给FirstActivity了。
+
+![image-20201113212315453](4-Homework.assets/image-20201113212315453.png)
+
+## 实验总结：
+
+### 评价：能够熟悉地了解到显示和隐式Intent和使用Intent在活动之间穿梭。
---
+++ 4-Homework/ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -0,0 +1,37 @@
+package com.example.activitytest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    public void onBackPressed() {//按下Back键返回
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "Hello FirstActivity");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Intent intent = getIntent();
+        //String data = intent.getStringExtra("extra_data");
+        //Log.d("SecondActivity", data);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "Hello FirstActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
