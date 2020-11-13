6d954244ca5d5017d53dc48845d6e90bbaa303a0
DESKTOP-CEKMC59\weiwudi <2374779862@qq.com>
2020-11-13 22:45:35 +0800

    no message

--- booksource/FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsContentActivity.java
+++ booksource/FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsContentActivity.java
@@ -19,5 +19,10 @@ public class NewsContentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_content);
+        String newsTitle =getIntent().getStringExtra("news_title");
+        String newsContent = getIntent().getStringExtra("news_content");
+        NewsContentFragment newsContentFragment = (NewsContentFragment)
+                getSupportFragmentManager().findFragmentById(R.id.news_content_fragment);
+        newsContentFragment.refresh(newsTitle, newsContent);
     }
 }
