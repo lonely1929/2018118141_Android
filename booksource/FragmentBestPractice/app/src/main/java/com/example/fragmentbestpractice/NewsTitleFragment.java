b376154f8d400a753d96eda8443db649e2b13ea7
DESKTOP-CEKMC59\weiwudi <2374779862@qq.com>
2020-11-13 23:47:06 +0800

    chapter4

--- booksource/FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
+++ booksource/FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
@@ -34,9 +34,9 @@ public class NewsTitleFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         if (getActivity().findViewById(R.id.news_content_layout) != null) {
-            isTwoPane = true; // 可以找到news_content_layout布局时，为双页模式
+            isTwoPane = true;
         } else {
-            isTwoPane = false; // 找不到news_content_layout布局时，为单页模式
+            isTwoPane = false; 
         }
     }
 
