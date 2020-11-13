07d01953629a1c03aaf0eb0bc19420b67b0159e6
DESKTOP-CEKMC59\weiwudi <2374779862@qq.com>
2020-11-13 22:42:28 +0800

    no message

--- booksource/FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsContentActivity.java
+++ booksource/FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsContentActivity.java
@@ -1,10 +1,19 @@
 package com.example.fragmentbestpractice;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewsContentActivity extends AppCompatActivity {
+
+    public static void actionStart(Context context, String newsTitle, String newsContent) {
+        Intent intent = new Intent(context, NewsContentActivity.class);
+        intent.putExtra("news_title", newsTitle);
+        intent.putExtra("news_content",newsContent);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
