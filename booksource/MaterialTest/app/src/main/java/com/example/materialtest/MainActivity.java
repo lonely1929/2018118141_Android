4c293b07c461c7eb8686371aaee521cc4fc7d527
DESKTOP-CEKMC59\weiwudi <2374779862@qq.com>
2020-12-24 12:16:15 +0800

    更新

--- booksource/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
+++ booksource/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
